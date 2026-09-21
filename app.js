6397336bb53391d67aac3ec73c05546660bd95f3
LucasVoss <lucasvoss2010@gmail.com>
2026-09-21 20:36:24 -0300

    Cria aplicativo

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log ("================================");
+console.log ("MEU APLICATIVO");
+console.log ("================================");
+console.log ("Nome: Lucas");
+console.log ("Cidade: Schroeder");
+console.log ("Atividade de interesse: Programação no geral e CyberSegurança");
+console.log ("Quero aprender: Me aprofundar na programação e na area da CyberSegurança");
+console.log ("Área de interesse: Programação e CyberSegurança");
+console.log ("Mensagem: Quero me Tornar um profissional de TI e me especializar na área de CyberSegurança");
