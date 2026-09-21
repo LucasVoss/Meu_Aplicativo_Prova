9d446a75eb3c0aa49b3cdf5e54c322361aaff5a7
LucasVoss <lucasvoss2010@gmail.com>
2026-09-21 20:43:19 -0300

    Adiciona novo desafio

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ console.log ("Atividade de interesse: Programação no geral e CyberSegurança")
 console.log ("Quero aprender: Me aprofundar na programação e na area da CyberSegurança");
 console.log ("Área de interesse: Programação e CyberSegurança");
 console.log ("Mensagem: Quero me Tornar um profissional de TI e me especializar na área de CyberSegurança");
+
+//Meu próximo desafio: Criar uma mensagem de boas vindas com seu nome!
+const prompt = require('prompt-sync')();
+const nome = prompt("Qual é o seu nome? ");
+console.log(`Olá, ${nome}! Bem-vindo ao meu aplicativo!`);
